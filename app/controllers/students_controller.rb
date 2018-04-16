a7707363e40c183a146b21d3bbc5e13938bf1d7c
Rocky Chiang <rocky@chem.ucla.edu>
2018-04-16 01:17:47 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,18 +1,23 @@
-class SchoolClassesController < ApplicationController
+class StudentsController < ApplicationController
 
   def new
   end
 
   def create
+    redirect_to students_path
   end
 
   def show
+    @student = Student.find(params[:id])
   end
 
   def edit
+    @student = Student.find(params[:id])
   end
 
   def update
+    @student = Student.find(params[:id])
+    redirect_to student_path(@student)
   end
 
 end
