39c4b3848c7878e3f09f3a51bbf4161b4d5b1dc2
Rocky Chiang <rocky@chem.ucla.edu>
2018-04-16 01:58:20 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -19,6 +19,7 @@ class SchoolClassesController < ApplicationController
 
   def update
     @school_class = SchoolClass.find(params[:id])
+    @school_class.update(params.require(:school_class).permit(:title, :room_number))
     redirect_to school_class_path(@school_class)
   end
 
