1be0326242432c4efa7507b0bdfc16dfbf180092
Rocky Chiang <rocky@chem.ucla.edu>
2018-04-16 01:57:45 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,10 +1,11 @@
 class SchoolClassesController < ApplicationController
 
   def new
-    @school_class = SchoolClass.new()
+    @school_class = SchoolClass.new
   end
 
   def create
+    @school_class = SchoolClass.create(params.require(:school_class).permit(:title, :room_number))
     redirect_to school_class_path(@school_class)
   end
 
